b9156f779d7182ef72ac198460706ddd8101e33b
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-08-10 22:02:00 +0700

    add i and s category

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -14,18 +14,26 @@ export class HomepageComponent implements OnInit {
   constructor(private productService: ProductService) {
     this.getAll();
   }
-  getAll() {
+  getAll(): Product[] {
     this.productService.getAllProduct().subscribe(products => {
       this.products = products;
       this.products.map(product => {
         product.createDate = new Date(product.createDate);
       })
     })
+    return this.products
   }
 
   delete(id){
-    this.productService.deleteProduct(id).subscribe();
-    this.getAll();
+    if(confirm("Bạn có thực sự muốn xóa?")){
+      this.productService.deleteProduct(id).subscribe(
+        next =>{this.products = this.getAll();
+        },
+        error => {
+          alert("error when delete user")
+        }
+      );
+    }
   }
 
   ngOnInit(): void {
